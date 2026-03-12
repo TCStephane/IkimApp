e8d08121614f3531698d1048e23b9e925d142462
ben-m-m <mainaben23@gmail.com>
2026-03-12 16:25:59 +0200

    Add log_payment function and update imports in main.py

--- models/contribution.py
+++ models/contribution.py
@@ -1,4 +1,5 @@
-class Contribution:
+from datetime import date
+"""class Contribution:
     def __init__(self, member_id, cycle_id, amount, date):
         self.member_id = member_id
         self.cycle_id = cycle_id
@@ -14,4 +15,41 @@ class Contribution:
 
     @staticmethod
     def get_all():
-        pass
+        pass"""
+
+def log_payment():
+    print("\n--- Log Member Payment ---")
+
+    member_id = input("Enter Member ID: ")
+    amount = float(input("Enter Amount: "))
+    payments = []
+
+    print("Payment Type:")
+    print("1. Savings")
+    print("2. Loan Repayment")
+    print("3. Fine")
+
+    choice = input("Select option: ")
+
+    if choice == "1":
+        payment_type = "Savings"
+    elif choice == "2":
+        payment_type = "Loan Repayment"
+    elif choice == "3":
+        payment_type = "Fine"
+    else:
+        print("Invalid option")
+        return
+
+    payment = {
+        "member_id": member_id,
+        "amount": amount,
+        "payment_type": payment_type,
+        "date": str(date.today())
+    }
+
+    payments.append(payment)
+
+    print("Payment recorded successfully.")
+
+log_payment()
