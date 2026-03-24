9c8b884476b35089f568d929d7ae7854e759a1f7
joshmulongo <j.mulongo@alustudent.com>
2026-03-24 16:49:48 +0200

    exclude .config from commits

--- config.py
+++ config.py
@@ -4,5 +4,5 @@ CONN_USER ="avnadmin"
 CONN_DB_NAME ="ikim_app"
 CONN_HOST ="mysql-13eaaecb-alustudent-76ac.c.aivencloud.com"
 CONN_PORT="24571"
-#CONN_PASS ="AVNS_pOKuGe1qW43ETMW8iDb"
+CONN_PASS ="AVNS_pOKuGe1qW43ETMW8iDb"
 CONN_SSL_DISABLED=False
